b0798521e44b1a8bdcf0077c2f98571988c6104e
Prakash <prakashkovaidt@gmail.com>
2019-10-22 18:39:15 +0530

    connecting angular and node

--- api_v1/routes/index.js
+++ api_v1/routes/index.js
@@ -7,6 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express'  });
 });
 router.post('/data', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   error = {
     status:"404",
     stack:"Try to access invalid resources"
