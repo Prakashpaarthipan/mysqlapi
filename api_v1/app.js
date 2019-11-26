83323afe86d0049d67757f0f73ec121d37c951f5
Prakash <prakashkovaidt@gmail.com>
2019-11-26 14:06:25 +0530

    ftpreadcheck

--- api_v1/app.js
+++ api_v1/app.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors')
+var cors = require('cors');
+var helmet = require('helmet');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
+var adminRouter = require('./routes/admin');
 var app = express();
 
 // view engine setup
@@ -18,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -27,6 +31,7 @@ app.use(function(req, res, next) {
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/v1', apiRouter);
+app.use('/pathfind/', adminRouter);
 var whitelist = [
   'http://0.0.0.0:3000',
   'http://localhost:4200'
